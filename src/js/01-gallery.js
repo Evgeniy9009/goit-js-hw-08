68a1be9c18a750f4722909d80fc4f6e05ddc1721
Evgeniy <evgeniy.grabar1@gmail.com>
2022-07-20 21:45:30 +0300

    add 1 exercise

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,19 @@
+import SimpleLightbox from "simplelightbox"
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery")
+console.log(gallery);
+
+const renderList = (products) => products.map(({ original, preview, description }) => `<a class="gallery__link" href="${original}"> <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a>`)
+    .join("");
+
+renderList(galleryItems)
+
+gallery.insertAdjacentHTML("beforeend"  , renderList(galleryItems))
+
+let galleryA = new SimpleLightbox('.gallery a', {captionsData : "alt",captionDelay : 250} );
