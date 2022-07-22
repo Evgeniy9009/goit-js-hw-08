dbb93d2f236d6b16a642682c1bce55c2e21098cc
Evgeniy <evgeniy.grabar1@gmail.com>
2022-07-22 22:53:03 +0300

    додав 2 завдання

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+let currentTime = 0
+LOCALSTOREG_TIME = "videoplayer-current-time"
+
+function getTime({seconds}) {
+    console.log('played the video!')
+
+    localStorage.setItem(LOCALSTOREG_TIME, seconds)
+    currentTime = localStorage.getItem(LOCALSTOREG_TIME)
+    console.log(currentTime)
+}
+    
+player.on('timeupdate', throttle(getTime, 1000));
+
+player.setCurrentTime(localStorage.getItem(LOCALSTOREG_TIME))
+
+
+
+
